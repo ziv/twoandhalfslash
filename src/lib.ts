f7acb580465a6bf1be33e8dd90b658386eb0a684
Ziv Perry <ziv.perry@zoominfo.com>
2025-09-19 14:52:00 +0300

    need to move to other machine commit

--- src/lib.ts
+++ src/lib.ts
@@ -1,145 +1,146 @@
-import type { Element, ElementContent, Text } from "hast";
-import type { ShikiTransformer } from "shiki";
+import type {Element, ElementContent, Text} from "hast";
+import type {ShikiTransformer} from "shiki";
+
+const TWOANDHALF = /\/\/\^ (.+)/;
 
 function text(
-  value: string,
+    value: string,
 ): Text {
-  return {
-    type: "text",
-    value,
-  };
+    return {
+        type: "text",
+        value,
+    };
 }
 
 function element(
-  tagName: string,
-  children: (Text | Element)[] = [],
-  properties: Record<string, string | string[]> = {},
+    tagName: string,
+    children: (Text | Element)[] = [],
+    properties: Record<string, string | string[]> = {},
 ): Element {
-  return {
-    type: "element",
-    tagName,
-    properties,
-    children,
-  };
+    return {
+        type: "element",
+        tagName,
+        properties,
+        children,
+    };
 }
 
 function ellipse(
-  width: number,
-  height: number,
-  strokeWidth: number,
-  stroke: string,
-  x: string,
-  y: string,
+    width: number,
+    height: number,
+    strokeWidth: number,
+    stroke: string,
+    x: string,
+    y: string,
 ): Element {
-  const rx = width / 2;
-  const ry = height / 2;
-  return element("svg", [
-    element("ellipse", [], {
-      cx: String(rx),
-      cy: String(ry),
-      rx: String(rx - strokeWidth),
-      ry: String(ry - strokeWidth),
-      stroke: stroke,
-      "stroke-width": strokeWidth + "px",
-      fill: "none",
-    }),
-  ], {
-    style: `transform: translateY(${x}); translateX(${y});`,
-    class: ["ths-ellipse"],
-    xmlns: "http://www.w3.org/2000/svg",
-    width: width + "px",
-    height: height + "px",
-    viewBox: `0 0 ${width} ${height}`,
-  });
+    const rx = width / 2;
+    const ry = height / 2;
+    return element("svg", [
+        element("ellipse", [], {
+            cx: String(rx),
+            cy: String(ry),
+            rx: String(rx - strokeWidth),
+            ry: String(ry - strokeWidth),
+            stroke: stroke,
+            "stroke-width": strokeWidth + "px",
+            fill: "none",
+        }),
+    ], {
+        style: `transform: translateY(${x}); translateX(${y});`,
+        class: ["ths-ellipse"],
+        xmlns: "http://www.w3.org/2000/svg",
+        width: width + "px",
+        height: height + "px",
+        viewBox: `0 0 ${width} ${height}`,
+    });
 }
 
 function update(el: Element, children: ElementContent[]) {
-  el.properties = {};
-  el.children = children;
+    el.properties = {};
+    el.children = children;
 }
 
 function transformFloating(
-  el: Element,
-  args: string[],
-  dir: "up" | "down" | "right" | "left" = "up",
+    el: Element,
+    args: string[],
+    dir: "up" | "down" | "right" | "left" = "up",
 ) {
-  const [content, options = ""] = args;
+    const [content, options = ""] = args;
 
-  const [x = 0, y = 0] = options.split(",");
+    const [x = 0, y = 0] = options.split(",");
 
-  update(
-    el,
-    [
-      element("span", [text(content)], {
-        class: ["ths-label", `ths-${dir}`],
-        style: `transform: translate(${x}, ${y});`,
-      }),
-    ],
-  );
+    update(
+        el,
+        [
+            element("span", [text(content)], {
+                class: ["ths-label", `ths-${dir}`],
+                style: `transform: translate(${x}, ${y});`,
+            }),
+        ],
+    );
 }
 
 function transformBanner(el: Element, args: string[]) {
-  const content = args[0];
-  update(
-    el,
-    [element("span", [text(content)], { class: ["ths-banner"] })],
-  );
+    const content = args[0];
+    update(
+        el,
+        [element("span", [text(content)], {class: ["ths-banner"]})],
+    );
 }
 
 function transformLink(line: Element, el: Element, args: string[]) {
-  const [search, target, label = ""] = args;
-  // search for the link in the line
-  // replace the link with a span with the class ths-link
+    const [search, target, label = ""] = args;
+    // search for the link in the line
+    // replace the link with a span with the class ths-link
 
-  for (const e of line.children) {
-    if (e.type !== "element") {
-      continue;
-    }
-    const first = e.children[0];
-    if (first?.type !== "text") {
-      continue;
-    }
-    // console.log(first);
-    if (!first.value.includes(search)) {
-      continue;
-    }
+    for (const e of line.children) {
+        if (e.type !== "element") {
+            continue;
+        }
+        const first = e.children[0];
+        if (first?.type !== "text") {
+            continue;
+        }
+        // console.log(first);
+        if (!first.value.includes(search)) {
+            continue;
+        }
 
-    let parts = first.value.split(search);
-    if (parts.length !== 1) {
-      // somthing bad happened
-      // nothing to do
-    }
+        let parts = first.value.split(search);
+        if (parts.length !== 1) {
+            // somthing bad happened
+            // nothing to do
+        }
 
-    update(
-      e,
-      [
-        element("span", [
-          text(parts[0]),
-        ]),
-        element("a", [
-          text(search),
-        ], {
-          href: target,
-          class: ["ths-link"],
-          title: label,
-        }),
-        element("span", [
-          text(parts[1]),
-        ]),
-      ],
-    );
-    // remove the comment
-    update(el, []);
-    break;
-  }
+        update(
+            e,
+            [
+                element("span", [
+                    text(parts[0]),
+                ]),
+                element("a", [
+                    text(search),
+                ], {
+                    href: target,
+                    class: ["ths-link"],
+                    title: label,
+                }),
+                element("span", [
+                    text(parts[1]),
+                ]),
+            ],
+        );
+        // remove the comment
+        update(el, []);
+        break;
+    }
 }
 
 function transformEllipse(el: Element, args: string[]) {
-  console.log({ el, args });
-  ellipse(1, 2, 3, "black", "0px", "0px");
+    console.log({el, args});
+    ellipse(1, 2, 3, "black", "0px", "0px");
 }
 
-const TWOANDHALF = /\/\/\^ (.+)/;
 
 /**
  * fetch the element that wrap the `//^` comment
@@ -147,77 +148,118 @@ const TWOANDHALF = /\/\/\^ (.+)/;
  * @param node
  */
 function getTwoandhalfslash(node: ElementContent): [Element, string] | null {
-  if (node.type !== "element") {
-    return null;
-  }
-  // is this the wrapper? let's check
-  const comment = node.children?.[0] as Text;
-  if (comment && comment.type === "text" && comment.value.includes("//^")) {
-    // yap, this is the wrapper object...
-    return [node, comment.value];
-  }
-  for (const child of node.children) {
-    const ret = getTwoandhalfslash(child);
-    if (ret) {
-      return ret;
+    if (node.type !== "element") {
+        return null;
     }
-  }
-  return null;
+    // is this the wrapper? let's check
+    const comment = node.children?.[0]; // as Text;
+    if (comment && comment.type === "text" && comment.value.includes("//^")) {
+        // yap, this is the wrapper object...
+        return [node, comment.value];
+    }
+    for (const child of node.children) {
+        const ret = getTwoandhalfslash(child);
+        if (ret) {
+            return ret;
+        }
+    }
+    return null;
 }
 
 function* lines(el: Element): Generator<Element> {
-  for (const child of el.children) {
-    if (child.type !== "element") {
-      continue;
+    for (const child of el.children) {
+        if (child.type !== "element") {
+            continue;
+        }
+        // child is line
+        yield child;
     }
-    // child is line
-    yield child;
-  }
 }
 
+function isNewLine(node: ElementContent): boolean {
+    return node && node.type === "text" && node.value === "\n";
+}
+
+function isElement(node: ElementContent): node is Element {
+    return node && node.type === "element";
+}
+
+function parse(str: string) {
+
+}
+
+
 export function twoandhalfslash(): ShikiTransformer {
-  return {
-    name: "twoandhalfs",
-    code(node: Element) {
-      for (const line of lines(node)) {
-        if (line.type !== "element") {
-          continue;
-        }
-        // linesStore.push(line);
-        const comment = getTwoandhalfslash(line);
-        if (!comment) {
-          continue;
-        }
-        const match = comment[1].match(TWOANDHALF);
-        if (!match) {
-          continue;
-        }
-        const [command, ...args] = match[1].split("^");
-        switch (command) {
-          case "e":
-            transformEllipse(comment[0], args);
-            break;
-          case "h":
-            transformLink(line, comment[0], args);
-            break;
-          case "b":
-            transformBanner(comment[0], args);
-            break;
-          case "d":
-            transformFloating(comment[0], args, "down");
-            break;
-          case "u":
-            transformFloating(comment[0], args, "up");
-            break;
-          case "r":
-            transformFloating(comment[0], args, "right");
-            break;
-          case "l":
-            transformFloating(comment[0], args, "left");
-            break;
-        }
-      }
-      return node;
-    },
-  };
+    return {
+        name: "twoandhalfs",
+        code(node: Element) {
+
+
+            const children: ElementContent[] = [];
+            for (let i = 0; i < node.children.length; i++) {
+                const line = node.children[i];
+                const newline = node.children[i + 1];
+
+                if (!isNewLine(newline)) { // last line handled
+                    continue;
+                }
+                if (!isElement(line)) {
+                    continue;
+                }
+                // search if line contains //^
+                const comment = getTwoandhalfslash(line);
+                if (!comment) {
+                    continue;
+                }
+                const match = comment[1].match(TWOANDHALF);
+                if (!match) {
+                    continue;
+                }
+                console.log(match[1]);
+                // console.log(line, readText(line));
+            }
+
+            console.log(node);
+            for (const line of lines(node)) {
+
+                if (line.type !== "element") {
+                    continue;
+                }
+                // linesStore.push(line);
+                const comment = getTwoandhalfslash(line);
+                if (!comment) {
+                    continue;
+                }
+                const match = comment[1].match(TWOANDHALF);
+                if (!match) {
+                    continue;
+                }
+                const [command, ...args] = match[1].split("^");
+                switch (command) {
+                    case "e":
+                        transformEllipse(comment[0], args);
+                        break;
+                    case "h":
+                        transformLink(line, comment[0], args);
+                        break;
+                    case "b":
+                        transformBanner(comment[0], args);
+                        break;
+                    case "d":
+                        transformFloating(comment[0], args, "down");
+                        break;
+                    case "u":
+                        transformFloating(comment[0], args, "up");
+                        break;
+                    case "r":
+                        transformFloating(comment[0], args, "right");
+                        break;
+                    case "l":
+                        transformFloating(comment[0], args, "left");
+                        break;
+                }
+            }
+            return node;
+        },
+    };
 }
---
+++ vite.lib.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts"; // Import the plugin
+
+export default defineConfig({
+  build: {
+    minify: false,
+    lib: {
+      formats: ["es"], // Specify the formats you want to build
+      entry: "src/lib.ts", // Your library's entry point
+      fileName: "lib", // Output file name
+    },
+  },
+  plugins: [dts()],
+});
