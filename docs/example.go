791e69a85f863c52168e55a06c9d95904ddc4524
Ziv Perry <ziv.perry@gmail.com>
2025-09-20 08:23:11 +0300

    preview

---
+++ docs/example.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time" //^ time package^left,1em,-.7em
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now()) //^ time object^up,-4em,1.5em
+}
