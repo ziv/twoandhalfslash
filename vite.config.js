565293da86f1e6e08f564bb18cbbeca37770780f
Ziv Perry <ziv.perry@gmail.com>
2025-09-20 08:28:31 +0300

    docs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts"; // Import the plugin
 
 export default defineConfig({
+    base: "/twoandhalfslash/"
   // build: {
   //   minify: false,
   //   lib: {
