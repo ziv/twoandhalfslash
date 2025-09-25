5f9bba5f2fa70b21fb3d463853b99f23289993d0
Ziv Perry <ziv.perry@zoominfo.com>
2025-09-25 13:35:27 +0300

    new docs

--- docs/example.js
+++
@@ -1,1 +0,0 @@
-const arr = new Int8Array(10); //^ l^Int8Array^url=https://developer.mozilla.org&label=Int8Array at MDN
--- docs/usage-vitepress.js
+++
@@ -1,9 +0,0 @@
-import {twoandhalfslash} from "twoandhalfslash";
-
-export default defineConfig({
-    markdown: {
-        codeTransformers: [
-            twoandhalfslash(), //^ add to markdown configuration^up,-6em,1.5em
-        ],
-    },
-});
